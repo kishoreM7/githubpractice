32886823bb23ccfc05412d4ed78e9e0eb69e135d
kishore <kishoremallarapu3@gmail.com>
2023-03-10 13:08:15 +0530

    second commit

--- src/test/java/practice/Practice.java
+++ src/test/java/practice/Practice.java
@@ -1,10 +1,24 @@
 package practice;
 
+import org.testng.annotations.Test;
+
 public class Practice {
-	public static void main(String[] args) {
+	@Test
+	public static void firsttest() {
 		System.out.println("hello man");
 		System.out.println("dhkfghedgsabcjln");
+		System.out.println("hi ra mallarapu");
 
+		System.out.println("hiii");
+	}
+
+	@Test
+	public static void secondtest() {
+		System.out.println("hello man");
+		System.out.println("dhkfghedgsabcjln");
+		System.out.println("hi ra mallarapu");
+
+		System.out.println("hiii");
 	}
 
 }
